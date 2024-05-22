bc9852971fe53e3415023fb3ce73c36ed8406d3c
LocalGit <107919154+Apauto-to-all@users.noreply.github.com>
2024-05-22 11:55:35 +0800

    添加全局配置文件

---
+++ config.py
@@ -0,0 +1,29 @@
+# 全局配置文件
+
+# 项目运行的主机和端口
+host = "localhost"
+port = 19764
+
+# 数据库配置
+db_name = "PostgreSQL"
+db_host = "localhost"
+db_port = 5432
+
+# 数据库
+database = "text"
+
+# 数据库root用户名和密码
+# root_uesr = "postgres"
+# root_password = "password"
+
+# 数据库查询用户名和密码
+query_user = "query"
+query_password = "password"
+
+# 数据库插入用户名和密码
+insert_user = "insert"
+insert_password = "password"
+
+# 数据库更新用户名和密码
+update_user = "update"
+update_password = "password"
