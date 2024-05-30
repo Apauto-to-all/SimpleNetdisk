c0a9c31fa53a123cdca8aa3c18093991ffb84d10
LocalGit <107919154+Apauto-to-all@users.noreply.github.com>
2024-05-30 16:13:26 +0800

    重命名文件和文件夹； 加密文件夹； 更新文档，添加相应的数据库操作；

--- routes/files.py
+++ routes/files.py
@@ -1,10 +1,10 @@
 import os
 from typing import Optional
-from fastapi import APIRouter, Cookie, Request
+from fastapi import APIRouter, Cookie, Form, Request
 from fastapi.responses import FileResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 import config
-from utils import user_utils, files_utils
+from utils import password_utils, user_utils, files_utils
 
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
@@ -47,9 +47,95 @@ async def delete_file(
     if not username:
         return {"error": "未登录"}
     # 获取被删除文件的父级文件夹id
-    parent_folder_id = files_utils.get_delete_file_parent_folder_id(username, file_id)
+    parent_folder_id = files_utils.delete_file_get_parent_folder_id(username, file_id)
     if parent_folder_id:
-        if parent_folder_id == "/":
-            return RedirectResponse(url="/index", status_code=303)
         return RedirectResponse(url=f"/index/{parent_folder_id}", status_code=303)
     return {"error": "删除失败"}
+
+
+# 重命名文件的路由
+@router.post("/rename")
+async def rename_file(
+    file_id: Optional[str] = None,  # 添加查询参数
+    folder_id: Optional[str] = None,  # 添加查询参数
+    new_file_name: Optional[str] = Form(None),  # 读取表单数据
+    new_folder_name: Optional[str] = Form(None),  # 读取表单数据
+    access_token: Optional[str] = Cookie(None),  # 读取 Cookie
+):
+    # 判断是否登录
+    username = user_utils.isLogin_getUser(access_token)
+    if not username:
+        return {"error": "未登录"}
+    if file_id and new_file_name:  # 重命名文件
+        parent_folder_id = files_utils.rename_file_get_parent_folder_id(
+            username,
+            file_id,
+            new_file_name,
+        )
+        if parent_folder_id:
+            return RedirectResponse(url=f"/index/{parent_folder_id}", status_code=303)
+    if folder_id and new_folder_name:  # 重命名文件夹
+        parent_folder_id = files_utils.rename_folder_get_parent_folder_id(
+            username,
+            folder_id,
+            new_folder_name,
+        )
+        if parent_folder_id:
+            return RedirectResponse(url=f"/index/{parent_folder_id}", status_code=303)
+
+    return {"error": "重命名失败"}
+
+
+# 加密文件夹
+@router.post("/encrypt")
+async def encrypt_folder(
+    folder_id: Optional[str] = None,  # 添加查询参数
+    password: Optional[str] = Form(None),  # 读取表单数据
+    access_token: Optional[str] = Cookie(None),  # 读取 Cookie
+):
+    # 判断是否登录
+    username = user_utils.isLogin_getUser(access_token)
+    if not username:
+        return {"error": "未登录"}
+    if folder_id and password:
+        parent_folder_id = files_utils.encrypt_folder_get_parent_folder_id(
+            username,
+            folder_id,
+            password,
+        )
+        if parent_folder_id:
+            return RedirectResponse(url=f"/index/{parent_folder_id}", status_code=303)
+    return {"error": "加密失败"}
+
+
+# 解密文件夹
+@router.post("/decrypt")
+async def decrypt_folder(
+    # 临时解密还是删除加密
+    folder_id: Optional[str] = None,  # 添加查询参数
+    is_temporary: Optional[bool] = Form(False),  # 读取表单数据
+    password: Optional[str] = Form(None),  # 读取表单数据
+    access_token: Optional[str] = Cookie(None),  # 读取 Cookie
+):
+    # 判断是否登录
+    username = user_utils.isLogin_getUser(access_token)
+    if not username:
+        return {"error": "未登录"}
+    if folder_id and password:
+        parent_folder_id = files_utils.decrypt_folder_get_parent_folder_id(
+            username,
+            folder_id,
+            password,
+            is_temporary,
+        )
+        if parent_folder_id:
+            response = RedirectResponse(
+                url=f"/index/{parent_folder_id}", status_code=303
+            )
+            if is_temporary:
+                response.set_cookie(
+                    key=f"folder-{folder_id}",
+                    value=password_utils.encrypt_password(folder_id),
+                )
+            return response
+    return {"error": "解密失败"}
--- utils/files_utils.py
+++ utils/files_utils.py
@@ -1,4 +1,7 @@
 # 验证文件夹id是否为用户的文件夹
+from utils import password_utils
+
+
 def verify_folder_is_user(username: str, folder_id: str) -> bool:
     """
     验证文件夹id是否为用户的文件夹
@@ -57,7 +60,7 @@ def get_folders_in_folder(username: str, folder_id: str) -> list:
 
 
 # 删除文件，获取父级文件夹id
-def get_delete_file_parent_folder_id(username: str, file_id: str) -> str:
+def delete_file_get_parent_folder_id(username: str, file_id: str) -> str:
     """
     删除文件
     :param username: 用户名
@@ -67,3 +70,102 @@ def get_delete_file_parent_folder_id(username: str, file_id: str) -> str:
     if username and file_id:
         return "/"
     return ""  # 删除失败
+
+
+# 获取父级文件夹id
+def get_parent_folder_id(username: str, folder_id: str) -> str:
+    """
+    获取父级文件夹id
+    :param username: 用户名
+    :param folder_id: 文件夹id
+    :return: 父级文件夹id
+    """
+    if username and folder_id:
+        return "/"
+    return ""  # 删除失败
+
+
+def rename_file_get_parent_folder_id(
+    username: str, file_id: str, new_file_name: str
+) -> str:
+    """
+    重命名文件
+    :param username: 用户名
+    :param file_id: 文件id
+    :param new_file_name: 新文件名
+    :return: 父级文件夹id
+    """
+    if username and file_id and new_file_name:
+        return "/"
+    return ""  # 重命名失败
+
+
+def rename_folder_get_parent_folder_id(
+    username: str, folder_id: str, new_folder_name: str
+) -> str:
+    """
+    重命名文件夹
+    :param username: 用户名
+    :param folder_id: 文件夹id
+    :param new_folder_name: 新文件夹名
+    :return: 父级文件夹id
+    """
+    if username and folder_id and new_folder_name:
+        return "/"
+    return ""  # 重命名失败
+
+
+def encrypt_folder_get_parent_folder_id(
+    username: str, folder_id: str, password: str
+) -> str:
+    """
+    加密文件夹
+    :param username: 用户名
+    :param folder_id: 文件夹id
+    :param password: 密码
+    :return: 父级文件夹id
+    """
+    if username and folder_id and password:
+        password = password_utils.encrypt_password(password)
+        return "/"
+    return ""  # 加密失败
+
+
+def get_folder_password(username: str, folder_id: str) -> str:
+    if username and folder_id:
+        return (
+            "$2b$12$RnKEz5vMMQW6zZ1/1fNntesE3dF88VDXHXa9XA4Mx7V6Ik.loMd0S"  # password
+        )
+    return ""  # 获取密码失败
+
+
+def rm_folder_password(username: str, folder_id: str) -> bool:
+    """
+    删除文件夹密码
+    :param username: 用户名
+    :param folder_id: 文件夹id
+    :return 如何删除密码成功就返回Treu，否则False
+    """
+    if username and folder_id:
+        return True
+    return False
+
+
+def decrypt_folder_get_parent_folder_id(
+    username: str, folder_id: str, password: str, is_temporary: bool
+) -> str:
+    """
+    解密文件夹
+    :param username: 用户名
+    :param folder_id: 文件夹id
+    :param password: 密码
+    :param is_temporary: 临时解密还是删除加密
+    :return: 父级文件夹id
+    """
+    if username and folder_id and password:
+        hashed_password = get_folder_password(username, folder_id)
+        if password_utils.verify_password(hashed_password, password):
+            if not is_temporary:  # 删除密码，永久实现
+                rm_folder_password(username, folder_id)  # 删除文件夹密码
+            return "/"
+    return ""  # 解密失败
