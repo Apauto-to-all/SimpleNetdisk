f4de182f1e5123e42ab0fc3327b6eb2c31a8d445
gorgeouskim <2369085561@qq.com>
2024-06-01 17:14:39 +0800

    上传创建用户sql文件

---
+++ db/sql文件/创建用户.sql
@@ -0,0 +1,11 @@
+-- 创建角色并赋予对应的权限
+CREATE ROLE select_update_insert_role;
+GRANT SELECT, INSERT, UPDATE 
+ON ALL TABLES IN SCHEMA 
+SimpleCloud TO select_update_insert_role;
+
+-- 创建用户并将角色授权给用户
+CREATE USER simple_cloud_user 
+WITH PASSWORD 'BiYCR@@Vbkk32$QnbT*VPR&x$Hiy6N';
+GRANT select_update_insert_role TO simple_cloud_user;
+
